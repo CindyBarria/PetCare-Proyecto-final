4a4cce46cb12e1ac3a4d9a84c1af993c84877dc1
cindybarria2-ops <cindybarria2@alumnos.cei.es>
2026-04-10 23:35:11 +0200

    feat:se agrega limite de imagen

--- backend/index.js
+++ backend/index.js
@@ -45,6 +45,10 @@ app.use(morgan('dev'));
 /* Parseo de JSON */
 app.use(express.json());
 
+/* Parseo de URL-encoded */
+app.use(express.json({ limit: '25mb' }));
+
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 /**
  * =========================================================
  * CONEXIÓN A BASE DE DATOS
